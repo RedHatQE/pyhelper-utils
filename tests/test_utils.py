b783e25951a62afab7eda83ce93f75cb605a239d
dbasunag <dbasunag@redhat.com>
2024-04-02 12:34:52 -0400

    add run_command

---
+++ tests/test_utils.py
@@ -0,0 +1,44 @@
+import shlex
+from subprocess import CalledProcessError
+import pytest
+
+from pyhelper_utils.utils import run_command
+from simple_logger.logger import get_logger
+from unittest.mock import MagicMock, patch
+
+LOGGER = get_logger(name=__name__)
+ERROR_MESSAGE = "Expected value {expected}, actual value {actual}"
+SUCCESSFUL_MESSAGE = "worked"
+FAILURE_MESSAGE = "No such file"
+
+
+def test_run_command_return_true():
+    rc, out, error = run_command(command=shlex.split(f"echo '{SUCCESSFUL_MESSAGE}'"), check=False)
+    LOGGER.warning(f"rc: {rc}, out: {out}, err: {error}")
+    assert rc, ERROR_MESSAGE.format(expected="0", actual="1")
+    assert not error, ERROR_MESSAGE.format(expected="", actual="error")
+    assert SUCCESSFUL_MESSAGE in out, ERROR_MESSAGE.format(expected=SUCCESSFUL_MESSAGE, actual=out)
+
+
+def test_run_command_return_false():
+    rc, out, error = run_command(command=shlex.split("false"), check=False)
+    LOGGER.warning(f"rc: {rc}, out: {out}, err: {error}")
+    assert not rc, ERROR_MESSAGE.format(expected="1", actual=rc)
+
+
+def test_run_command_no_verify_raises_exception():
+    with pytest.raises(CalledProcessError):
+        run_command(command=shlex.split("false"), check=True, verify_stderr=False)
+
+
+@patch("pyhelper_utils.utils.subprocess.run")
+def test_run_command_error(mock_run):
+    mock_out = MagicMock()
+    mock_out.configure_mock(**{"stdout": "", "stderr": FAILURE_MESSAGE, "returncode": 0})
+
+    mock_run.return_value = mock_out
+    rc, out, error = run_command(command=shlex.split("true"), capture_output=False, check=False, shell=True)
+    LOGGER.warning(f"rc: {rc}, out: {out}, err: {error}")
+    assert not rc, ERROR_MESSAGE.format(expected="0", actual="1")
+    assert FAILURE_MESSAGE in error, ERROR_MESSAGE.format(expected=FAILURE_MESSAGE, actual="error")
+    assert not out, ERROR_MESSAGE.format(expected="", actual=out)
