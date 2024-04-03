7233ff6599bbc4fda555f6aa77abab42aaf0c7ff
dbasunag <dbasunag@redhat.com>
2024-04-03 19:30:10 -0400

    add run_ssh_command

---
+++ pyhelper_utils/exceptions.py
@@ -0,0 +1,7 @@
+class CommandExecFailed(Exception):
+    def __init__(self, name, err=None):
+        self.name = name
+        self.err = f"Error: {err}" if err else ""
+
+    def __str__(self):
+        return f"Command: {self.name} - exec failed. {self.err}"
