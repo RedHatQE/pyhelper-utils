2b43b8222f7381c7004bbc4dc17f84409e6b8cc2
Meni Yakove <441263+myakove@users.noreply.github.com>
2024-04-03 10:51:19 +0300

    Add ignore_exceptions function (#4)
    * Add ignore_exceptions func
    
    * fix typo
    
    * add pkg rules to renovate
    
    * add pkg rules to renovate

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,7 +1,22 @@
-from pyhelper_utils.general import tts
+from pyhelper_utils.general import tts, ignore_exceptions
+import pytest
+import logging
+
+
+@pytest.fixture
+def func_for_ignore_exception():
+    @ignore_exceptions(logger=logging.getLogger(), retry=1)
+    def _foo():
+        raise ValueError()
+
+    return _foo
 
 
 def test_tts():
     assert tts("1m") == 60
     assert tts("1h") == 3600
     assert tts("3600") == 3600
+
+
+def test_ignore_exceptions(func_for_ignore_exception):
+    assert not isinstance(func_for_ignore_exception(), Exception)
