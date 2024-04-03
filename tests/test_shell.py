1c08db55f4a1f92373972e99a1acf5f7cf8d9bef
Debarati Basu-Nag <dbasunag@redhat.com>
2024-04-03 11:32:13 -0400

    add run_command (#6)
    * add run_command
    
    * fix minor things
    
    * review comments addressed
    
    * Update pyhelper_utils/shell.py
    
    Co-authored-by: Meni Yakove <441263+myakove@users.noreply.github.com>
    
    * remove verbose from tox
    
    * address review comments
    
    * fix tox failure
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Meni Yakove <441263+myakove@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/test_shell.py
@@ -0,0 +1,37 @@
+import shlex
+from subprocess import CalledProcessError
+import pytest
+import subprocess
+from pyhelper_utils.shell import run_command
+
+ERROR_MESSAGE = "Expected value {expected}, actual value {actual}"
+SUCCESSFUL_MESSAGE = "worked"
+FAILURE_MESSAGE = "No such file"
+
+
+def test_run_command_return_true():
+    rc, out, error = run_command(command=shlex.split(f"echo '{SUCCESSFUL_MESSAGE}'"), check=False)
+    assert rc, ERROR_MESSAGE.format(expected=True, actual=rc)
+    assert not error, ERROR_MESSAGE.format(expected="", actual="error")
+    assert SUCCESSFUL_MESSAGE in out, ERROR_MESSAGE.format(expected=SUCCESSFUL_MESSAGE, actual=out)
+
+
+def test_run_command_return_false():
+    rc, _, _ = run_command(command=shlex.split("false"), check=False)
+    assert not rc, ERROR_MESSAGE.format(expected=False, actual=rc)
+
+
+def test_run_command_no_verify_raises_exception():
+    with pytest.raises(CalledProcessError):
+        run_command(command=shlex.split("false"), check=True, verify_stderr=False)
+
+
+def test_run_command_error(mocker):
+    mocker.patch(
+        "pyhelper_utils.shell.subprocess.run",
+        return_value=subprocess.CompletedProcess(args=None, stderr=FAILURE_MESSAGE, returncode=0, stdout=""),
+    )
+    rc, out, error = run_command(command=shlex.split("true"), capture_output=False, check=False, shell=True)
+    assert not rc, ERROR_MESSAGE.format(expected=False, actual=rc)
+    assert FAILURE_MESSAGE in error, ERROR_MESSAGE.format(expected=FAILURE_MESSAGE, actual="error")
+    assert not out, ERROR_MESSAGE.format(expected="", actual=out)
