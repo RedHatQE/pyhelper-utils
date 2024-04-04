6cb8e7e3767debad5b6076d113a3bd4fda3f671a
Debarati Basu-Nag <dbasunag@redhat.com>
2024-04-04 06:50:52 -0400

    Update pyhelper_utils/shell.py
    Co-authored-by: Meni Yakove <441263+myakove@users.noreply.github.com>

--- pyhelper_utils/shell.py
+++ pyhelper_utils/shell.py
@@ -81,7 +81,7 @@ def run_ssh_commands(
     get_pty: bool = False,
     check_rc: bool = True,
     timeout: int = TIMEOUT_30MIN,
-    tcp_timeout: float = None,
+    tcp_timeout: Optional[float] = None,
 ) -> list:
     """
     Run commands on remote host via SSH
